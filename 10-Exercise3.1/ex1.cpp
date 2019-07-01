7bb665073a5db098e4a19b938e4b1182bd9b01ce
Quang Tran <nhatquang304@gmail.com>
2019-07-01 15:33:56 -0400

    Exercise 10: Write code using the 'using' declaration.

---
+++ 10-Exercise3.1/ex1.cpp
@@ -0,0 +1,16 @@
+/*
+ *      Author: Quang Tran
+ *      Date: July 1, 2019
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    int sum = 0;
+    for (int val = 1; val <= 10; ++val)     sum += val;
+    cout << "Sum of 1 to 10 inclusive is " << sum << endl;
+    return 0;
+}
