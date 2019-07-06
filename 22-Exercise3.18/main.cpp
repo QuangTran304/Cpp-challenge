69b1e693d433d3363aca2f7efe32e60ae7eca734
Quang Tran <nhatquang304@gmail.com>
2019-07-05 21:15:02 -0400

    Exercise 22: Fix error from a given code.

---
+++ 22-Exercise3.18/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *      Author: Quang Tran
+ *      Date: July 5, 2019
+ */
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    // The program given is invalid. The vector needs to be initialized.
+    // Solution:
+
+    std::vector<int> ivec{42};
+
+    return 0;
+}
