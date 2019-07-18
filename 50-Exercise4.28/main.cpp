597272c28d2aba910ea39d21aff10adb8476bf0b
Quang Tran <nhatquang304@gmail.com>
2019-07-18 15:04:32 -0400

    Exercise 50: sizeof() operator

---
+++ 50-Exercise4.28/main.cpp
@@ -0,0 +1,21 @@
+/*
+ *      Author: Quang Tran
+ *      Date: July 18, 2019
+ */
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+
+    cout << "Size of a bool is: " << sizeof(bool) << " bytes" << endl;
+    cout << "Size of a char is: " << sizeof(char) << " bytes" << endl;
+    cout << "Size of a short int is: " << sizeof(short int) << " bytes" << endl;
+    cout << "Size of an int is: " << sizeof(int) << " bytes" << endl;
+    cout << "Size of a long int is: " << sizeof(long int) << " bytes" << endl;
+    cout << "Size of a float is: " << sizeof(float) << " bytes" << endl;
+    cout << "Size of a double is: " << sizeof(double) << " bytes" << endl;
+    return 0;
+}
