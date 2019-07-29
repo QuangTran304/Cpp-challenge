d6c0c420423b7638b022e246c9b063566d7ec9d8
Quang Tran <nhatquang304@gmail.com>
2019-07-29 15:13:40 -0400

    Exercise 63: Simple division program.

---
+++ 63-Exercise5.23/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *      Author: Quang Tran
+ *      Date: July 29, 2019
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Please enter two integers: " << std::endl;
+    int a, b;
+    std::cin >> a >> b;
+
+    std::cout << "The result is: " << (a / b) << std::endl;
+
+    return 0;
+}
