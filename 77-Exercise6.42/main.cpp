23d693069486af1a80935ef110915ba067e9637b
Quang Tran <nhatquang304@gmail.com>
2019-08-02 18:59:22 -0400

    Exercise 77: Default arguments.

---
+++ 77-Exercise6.42/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *      Author: Quang Tran
+ *      Date: August 2, 2019
+ */
+
+#include <iostream>
+
+std::string make_plural(size_t ctr, const std::string& word, const std::string& ending = "s") {
+    return (ctr > 1) ? word + ending : word;
+}
+
+int main() {
+    std::cout << "success: " << make_plural(2, "success", "es") << std::endl;
+    std::cout << "failure: " << make_plural(2, "failure") << std::endl;
+    return 0;
+}
