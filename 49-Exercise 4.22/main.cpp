3c37139c2592b1a32b921b9e7fb5a1ec721f0ad7
Quang Tran <nhatquang304@gmail.com>
2019-08-05 17:23:55 -0400

    Exercise 80: Function matching.

--- 49-Exercise 4.22/main.cpp
+++ 49-Exercise 4.22/main.cpp
@@ -5,17 +5,17 @@
  *      Note: This is version 1 of the exercise
  */
 
-//#include <iostream>
-//
-//using std::cout;
-//using std::endl;
-//using std::cin;
-//
-//int main() {
-//    int grade = 0;
-//    cout << "Please enter a grade: ";
-//    cin >> grade;
-//    cout << (  (grade > 90) ? "high pass" : (grade >= 60 && grade <= 75) ? "low pass" : "fail");
-//
-//    return 0;
-//}
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::cin;
+
+int main() {
+    int grade = 0;
+    cout << "Please enter a grade: ";
+    cin >> grade;
+    cout << (  (grade > 90) ? "high pass" : (grade >= 60 && grade <= 75) ? "low pass" : "fail");
+
+    return 0;
+}
