68d8034728eaa56851ab1d65d87e4f70cf7b9a91
Quang Tran <nhatquang304@gmail.com>
2019-08-07 16:37:10 -0400

    Exercise 85: Simple 'Person' class.

---
+++ 85-Exercise7.4/main.cpp
@@ -0,0 +1,16 @@
+/*
+ *      Author: Quang Tran
+ *      Date: August 7, 2019
+ */
+
+#include <iostream>
+
+struct Person {
+    std::string name;
+    std::string address;
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
